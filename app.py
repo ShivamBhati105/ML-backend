e88d77a111d92f9d557af7263f8b3eb552797ca5
Shivambhati105 <shivambhatinew@example.com>
2025-05-19 23:38:57 +0530

    Final Backend

--- app.py
+++ app.py
@@ -248,5 +248,5 @@ def execute_code():
         
     return jsonify(response)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000, debug=True)
